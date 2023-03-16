74e2811b95752d84d66468bf3d2e6c181399d785
Tarek Chaaban <Chaaban_tarek@yahoo.com>
2023-03-16 23:18:02 +0000

    completed exploring the waters.

--- CodeSignal/arrayChange.js
+++ CodeSignal/arrayChange.js
@@ -1,1 +1,13 @@
 /* eslint-disable no-unused-vars */
+function solution(inputArray) {
+  let moves = 0;
+
+  for (let i = 1; i < inputArray.length; i++) {
+    if (inputArray[i] <= inputArray[i - 1]) {
+      moves += inputArray[i - 1] - inputArray[i] + 1;
+      inputArray[i] = inputArray[i - 1] + 1;
+    }
+  }
+
+  return moves;
+}
