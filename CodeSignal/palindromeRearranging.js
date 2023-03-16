ca389be15a20d3135167b1e315f018b63219ab1d
Tarek Chaaban <Chaaban_tarek@yahoo.com>
2023-03-16 23:22:55 +0000

    completed exploring the waters.

--- CodeSignal/palindromeRearranging.js
+++ CodeSignal/palindromeRearranging.js
@@ -1,1 +1,22 @@
 /* eslint-disable no-unused-vars */
+/* function solution(inputString) {
+  var charArray = inputString.split('');
+
+  var letterObject = {};
+
+  for (var i = 0; i < charArray.length; i++) {
+    if (!letterObject.hasOwnProperty(charArray[i])) {
+      letterObject[charArray[i]] = 1;
+    } else {
+      letterObject[charArray[i]]++;
+    }
+  }
+
+  var oddCount = 0;
+  for (var key in letterObject) {
+    if (letterObject[key] % 2 !== 0) {
+      oddCount++;
+    }
+  }
+  return !(oddCount > 1);
+} */
