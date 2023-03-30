ef44f773baeb6936d3b57642bb0aaae73d5cae72
Tarek Chaaban <Chaaban_tarek@yahoo.com>
2023-03-30 00:21:49 +0000

    completed avoid obstacles.

--- CodeSignal/avoidObstacles.js
+++ CodeSignal/avoidObstacles.js
@@ -1,1 +1,16 @@
 /* eslint-disable no-unused-vars */
+function solution(inputArray) {
+  inputArray = inputArray.sort((a, b) => {
+    return a - b;
+  });
+
+  var lastArrayVal = inputArray[inputArray.length - 1];
+
+  for (var i = 1; i <= lastArrayVal + 1; i++) {
+    if (inputArray.every(element => {
+      return element % i !== 0;
+    })) {
+      return i;
+    }
+  }
+}
