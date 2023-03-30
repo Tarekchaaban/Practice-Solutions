e39db58000bf4fa0ce2acf8e0aa20237ee5a1ced
Tarek Chaaban <Chaaban_tarek@yahoo.com>
2023-03-30 22:09:18 +0000

    completed runningSum.

--- LeetCode/runningSum.js
+++ LeetCode/runningSum.js
@@ -1,1 +1,11 @@
 /* eslint-disable no-unused-vars */
+var runningSum = function (nums) {
+  const runningSumArray = [];
+  let runningSum = 0;
+  for (let i = 0; i < nums.length; i++) {
+    runningSum += nums[i];
+    runningSumArray.push(runningSum);
+  }
+  return runningSumArray;
+
+};
