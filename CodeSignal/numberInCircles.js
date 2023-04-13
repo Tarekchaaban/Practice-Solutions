63df72c25e1251b798f021bd352bcb3218abcdcd
Tarek Chaaban <Chaaban_tarek@yahoo.com>
2023-04-13 23:37:20 +0000

    completed circle of numbers.

--- CodeSignal/numberInCircles.js
+++ CodeSignal/numberInCircles.js
@@ -1,1 +1,12 @@
 /* eslint-disable no-unused-vars */
+function solution(n, firstNumber) {
+  const halfOfN = n / 2;
+  let oppositeNumber = 0;
+  if (firstNumber >= halfOfN) {
+    oppositeNumber = firstNumber - halfOfN;
+  } else {
+    oppositeNumber = firstNumber + halfOfN;
+  }
+  return oppositeNumber;
+
+}
