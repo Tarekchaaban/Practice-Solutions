aca0db9a3e67e6fe0d0c95f8add856e549842930
Tarek Chaaban <Chaaban_tarek@yahoo.com>
2023-04-27 02:05:27 +0000

    completed is Digit.

--- CodeSignal/isDigit.js
+++ CodeSignal/isDigit.js
@@ -1,1 +1,8 @@
 /* eslint-disable no-unused-vars */
+function solution(inputString) {
+  for (let i = 0; i < inputString.length; i++) {
+    if (!isNaN(inputString[i]) && inputString[i] !== ' ') {
+      return inputString[i];
+    }
+  }
+}
