15db6d11eb525d1c32fb31f0f9ad5a22378e1529
Tarek Chaaban <Chaaban_tarek@yahoo.com>
2023-04-27 22:12:03 +0000

    completed array Max Consecutive.

--- CodeSignal/arrayMaxConsecutive.js
+++ CodeSignal/arrayMaxConsecutive.js
@@ -1,1 +1,15 @@
 /* eslint-disable no-unused-vars */
+function solution(inputArray, k) {
+  let sum = 0;
+  for (let i = 0; i < k; i++) {
+    sum += inputArray[i];
+  }
+  let maxSum = sum;
+  for (let i = k; i < inputArray.length; i++) {
+    sum = sum - inputArray[i - k] + inputArray[i];
+    if (sum > maxSum) {
+      maxSum = sum;
+    }
+  }
+  return maxSum;
+}
