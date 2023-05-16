a33065d1e905144942706127b313d2212918defd
Tarek Chaaban <Chaaban_tarek@yahoo.com>
2023-05-16 00:10:42 +0000

    completed longestDigitsPrefix.

--- CodeSignal/longestDigitsPrefix.js
+++ CodeSignal/longestDigitsPrefix.js
@@ -1,1 +1,12 @@
 /* eslint-disable no-unused-vars */
+function solution(inputString) {
+  let longestPrefix = '';
+  for (let i = 0; i < inputString.length; i++) {
+    const char = inputString[i];
+    if (isNaN(char) || inputString[0] === ' ') {
+      break;
+    }
+    longestPrefix += inputString[i];
+  }
+  return longestPrefix;
+}
