c2fd7c4cccee9d83a078ec4d1f5f20a62fb6c5b5
Tarek Chaaban <Chaaban_tarek@yahoo.com>
2023-05-16 21:37:44 +0000

    completed digit degree.

--- CodeSignal/digitDegree.js
+++ CodeSignal/digitDegree.js
@@ -1,1 +1,22 @@
 /* eslint-disable no-unused-vars */
+function solution(n) {
+  let degree = 0;
+
+  while (n >= 10) {
+    n = sumDigits(n);
+    degree++;
+  }
+
+  return degree;
+}
+
+function sumDigits(n) {
+  let sum = 0;
+
+  while (n > 0) {
+    sum += n % 10;
+    n = Math.floor(n / 10);
+  }
+
+  return sum;
+}
