78ab69e182d167764a38211474e8ce5a9afc5435
Tarek Chaaban <Chaaban_tarek@yahoo.com>
2023-05-24 05:06:00 +0000

    completed candies.

--- CodeSignal/candies.js
+++ CodeSignal/candies.js
@@ -1,1 +1,12 @@
 /* eslint-disable no-unused-vars */
+function solution(n, m) {
+  const amountToEat = Math.floor(m / n);
+  let kidsFed = 0;
+  let totalEaten = 0;
+  while (kidsFed < n) {
+    totalEaten += amountToEat;
+    kidsFed++;
+  }
+  return totalEaten;
+
+}
