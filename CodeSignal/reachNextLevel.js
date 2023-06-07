f9f86143d9f9f785b000b1975cdea33c813074b2
Tarek Chaaban <Chaaban_tarek@yahoo.com>
2023-06-07 00:51:20 +0000

    completed reach next level.

--- CodeSignal/reachNextLevel.js
+++ CodeSignal/reachNextLevel.js
@@ -1,1 +1,9 @@
 /* eslint-disable no-unused-vars */
+function solution(experience, threshold, reward) {
+  if (experience + reward >= threshold) {
+    return true;
+  } else {
+    return false;
+  }
+
+}
