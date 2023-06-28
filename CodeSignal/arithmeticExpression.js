3c0403886530e0cf260761763acf44a0620d679c
Tarek Chaaban <Chaaban_tarek@yahoo.com>
2023-06-28 02:52:21 +0000

    completed arithmetic Expression.

--- CodeSignal/arithmeticExpression.js
+++ CodeSignal/arithmeticExpression.js
@@ -1,1 +1,9 @@
 /* eslint-disable no-unused-vars */
+function solution(a, b, c) {
+  if (a + b === c || a - b === c || a * b === c || a / b === c) {
+    return true;
+  } else {
+    return false;
+  }
+
+}
