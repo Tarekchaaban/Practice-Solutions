db518beaf07194357b6ff2b1a8a6bb0f90dd399d
Tarek Chaaban <Chaaban_tarek@yahoo.com>
2023-07-10 22:25:27 +0000

    completed willYou

--- CodeSignal/willYou.js
+++ CodeSignal/willYou.js
@@ -1,1 +1,11 @@
 /* eslint-disable no-unused-vars */
+function solution(young, beautiful, loved) {
+  if ((loved === true) && (young === false || beautiful === false)) {
+    return true;
+  } else if ((loved === false) && (young === true && beautiful === true)) {
+    return true;
+  } else {
+    return false;
+  }
+
+}
