a430d0ad10cb5155efbc64a7ced4aceeca9528fa
Tarek Chaaban <Chaaban_tarek@yahoo.com>
2023-07-11 22:42:41 +0000

    completed leastFactorial.

--- CodeSignal/leastFactorial.js
+++ CodeSignal/leastFactorial.js
@@ -1,1 +1,13 @@
 /* eslint-disable no-unused-vars */
+function solution(n) {
+  let currentFactorial = 1;
+  for (let i = 1; i < 100; i++) {
+    if (currentFactorial < n) {
+      currentFactorial = currentFactorial * (i + 1);
+    } else {
+      break;
+    }
+  }
+  return currentFactorial;
+
+}
