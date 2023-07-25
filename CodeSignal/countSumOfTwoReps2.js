0fb0c4c530ca44c6a45cfd1e7259d8bca1e79240
Tarek Chaaban <Chaaban_tarek@yahoo.com>
2023-07-25 06:31:12 +0000

    completed countSum 2.

--- CodeSignal/countSumOfTwoReps2.js
+++ CodeSignal/countSumOfTwoReps2.js
@@ -1,1 +1,11 @@
 /* eslint-disable no-unused-vars */
+function solution(n, l, r) {
+  var count = 0;
+
+  for (var i = l; i <= r; i++) {
+    if (i <= (n - i) && (n - i) <= r) {
+      count++;
+    }
+  }
+  return count;
+}
